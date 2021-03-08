af4df8274e03ece2106e27bfb29a1c0fdce0c67a
irunwithneedles <80079484+irunwithneedles@users.noreply.github.com>
2021-03-08 01:51:19 -0600

    Restructured Code

--- bungiefetch.js
+++ bungiefetch.js
@@ -1,14 +1,30 @@
 var apiKey = "2f0369fc936941f7a71542fa95f592af";
+var rootPath = "https://www.bungie.net/Platform/";
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/Clan/3910069/WeeklyRewardState/", true);
+
+//Xur Linkage
+var membershipType = "Account";
+var destinyMembershipId = "";
+var characterId = "";
+var vendorHash = "";
+
+var xhr = new XMLHttpRequest();	
+
+xhr.open("GET", rootPath + "Destiny2/" + membershipType + "/Profile/"+ destinyMembershipId + "/Character/" + characterId + "/Vendors/" + vendorHash + "/", true);
 xhr.setRequestHeader("X-API-Key", apiKey);
 
 xhr.onreadystatechange = function(){
- if(this.readyState === 4 && this.status === 200){
-  var json = JSON.parse(this.responseText);
-  console.log(json.Response.data); //Gjallarhorn
- }
+	if(this.readyState === 4 && this.status === 200){
+		var reply = JSON.parse(this.responseText);
+		document.getElementById("Xur").innerHTML = reply.ErrorStatus;
+		console.log(reply.ErrorStatus); //Gjallarhorn
+	}
 }
-xhr.open("GET","clanstatus.txt", true);
 xhr.send();
+
+//Vendors: 400
+//When obtaining vendor information, this will return summary information about the Vendor or Vendors being returned.
+//VendorCategories: 401
+//When obtaining vendor information, this will return information about the categories of items provided by the Vendor.
+//VendorSales: 402
+//When obtaining vendor information, this will return the information about items being sold by the Vendor.
